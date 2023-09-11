776325af26990faf97ca25cf480b58342451892d
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-11 12:10:48 +0330

    app created

--- tattoshop/wsgi.py
+++ tattoshop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for home_app project.
+WSGI config for tattoshop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tattoshop.settings')
 
 application = get_wsgi_application()
