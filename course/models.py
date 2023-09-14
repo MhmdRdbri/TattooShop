34870356ad749c1f7b83e0970abb0e41a7fd20c6
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-14 13:56:14 +0330

    Fix changes in course model dynamic attrs

--- course/models.py
+++ course/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="نام دوره")
+
+    class Meta:
+        verbose_name = 'دوره'
+        verbose_name_plural = "دوره ها"
+
 
     def __str__(self):
         return self.name
@@ -10,8 +15,12 @@ class Course(models.Model):
 
 class CourseAttribute(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    key = models.CharField(max_length=100)
-    value = models.CharField(max_length=255)
+    key = models.CharField(max_length=100, verbose_name="نام ویژگی")
+    value = models.CharField(max_length=255, verbose_name="مقدار ویژگی")
 
     def __str__(self):
         return f"{self.course.name} - {self.key}: {self.value}"
+
+    class Meta:
+        verbose_name = 'ویژگی'
+        verbose_name_plural = "ویژگی ها"
