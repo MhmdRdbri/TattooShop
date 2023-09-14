09dfea8ac38f9b012227e6dba97c1c27bc73805e
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-14 13:10:57 +0330

    important changes in course model

--- course/admin.py
+++ course/admin.py
@@ -5,16 +5,3 @@ from django.contrib import admin
 from .models import Course
 
 
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-    fieldsets = (
-        (None, {
-            'fields': ('name',)
-        }),
-        ('Dynamic Attributes', {
-            'fields': ('dynamic_attributes',),
-            'classes': ('collapse',),
-        }),
-    )
--- course/models.py
+++ course/models.py
@@ -3,7 +3,14 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
-    dynamic_attributes = models.JSONField()
+    attributes = models.ManyToManyField('CourseAttribute', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class CourseAttribute(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
