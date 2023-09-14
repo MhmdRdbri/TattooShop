86dd3b1764d747df69097c89ec156102077ed93e
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-14 13:39:28 +0330

    important changes in course admin.py and add new method for attributes in course models.py

--- course/models.py
+++ course/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
-from django.db import models
-
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
-    dynamic_attributes = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class CourseAttribute(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    key = models.CharField(max_length=100)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.course.name} - {self.key}: {self.value}"
