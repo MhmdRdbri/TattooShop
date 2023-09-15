2d883f5a6a20ef9b63483b0053ce3711e52e27c1
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-15 12:11:05 +0330

    New model added --> Samples

--- samples/models.py
+++ samples/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
-# Create your models here.
+
+class Samples(models.Model):
+    Author = (
+        ('Z', 'ضیائی'),
+        ('K', 'کارآموز'),
+    )
+
+    name = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=1, choices=Author, default='Z')
+    description = CKEditor5Field('Text', config_name='extends')
+    video = models.FileField(upload_to='video/samples')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
