cca827c75a69dfb81ce4e39d9ca5c0b4f794de84
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-15 12:53:18 +0330

    some changes --> Samples

--- samples/models.py
+++ samples/models.py
@@ -2,19 +2,32 @@ from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Samples(models.Model):
     Author = (
         ('Z', 'ضیائی'),
         ('K', 'کارآموز'),
     )
 
-    name = models.CharField(max_length=250)
-    title = models.CharField(max_length=250)
-    author = models.CharField(max_length=1, choices=Author, default='Z')
-    description = CKEditor5Field('Text', config_name='extends')
-    video = models.FileField(upload_to='video/samples')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=250, verbose_name='نام طرح')
+    title = models.CharField(max_length=250, verbose_name='عتوان طرح')
+    category = models.ManyToManyField(Category, related_name="samples", verbose_name='Category')
+    author = models.CharField(max_length=1, choices=Author, default='Z', verbose_name="توسط")
+    description = CKEditor5Field('توضیحات', config_name='extends')
+    video = models.FileField(upload_to='video/samples', verbose_name='ویدیو طرح')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='آپدیت')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'طرح'
+        verbose_name_plural = 'طرح ها'
