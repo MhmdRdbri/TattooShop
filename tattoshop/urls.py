b40e09079f1bda6a12520e452865e1c45120925a
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-17 17:13:32 +0330

    change database and migrate all models

--- tattoshop/urls.py
+++ tattoshop/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home_app.urls', namespace='home_app')),
-    path('', include('blog.urls')),
-    path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('home_app.urls', namespace='home_app')),
+                  path('', include('blog.urls')),
+                  path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                                                           document_root=settings.MEDIA_ROOT)
