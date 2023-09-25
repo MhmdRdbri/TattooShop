8268c8a5509528d6566811aad1416c1806dab239
MhmdRdbri <mohammadroodbari2019@gmail.com>
2023-09-25 14:00:33 +0330

    models updated --> Samples templates and views updated

--- samples/admin.py
+++ samples/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import *
+from django import forms
+
+
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('__all__',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj:  # If an object already exists, allow editing only
+            kwargs['form'] = TagsAdminForm
+        return super().get_form(request, obj, **kwargs)
+
+    def has_add_permission(self, request):
+        # Check if there is already an existing product
+        return not Tags.objects.exists()
+
+
+class TagsAdminForm(forms.ModelForm):
+    class Meta:
+        model = Tags
+        fields = ['__all', ]
+
 
 admin.site.register(Samples)
 admin.site.register(SamplesCategory)
+admin.site.register(Tags)
